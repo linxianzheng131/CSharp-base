eafb4570f6b86ae1e5012958a55d3295d987bf3e
XianZheng Lin <2520505328@stu.gdpu.edu.cn>
2026-09-13 09:56:55 +0800

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Test
+{
+    public class Father
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("父亲说话了");
+        }
+    }
+    public class Son : Father
+    {
+        public void Speak()
+        {
+            Console.WriteLine("儿子说话了");
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Father son = new Son();
+            son.Speak();
+            (son as Son).Speak();
+        }
+    }
+}
