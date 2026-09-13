c508c90a9ce1d9a9f8c603ea2b2e86c32f08807e
XianZheng Lin <2520505328@stu.gdpu.edu.cn>
2026-09-13 09:54:46 +0800

    Add files via upload

---
+++ lesson13_练习题/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Lesson13_练习题
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("冒泡排序练习题");
+
+            #region 练习题一
+            //定义一个数组，长度为20，每个元素值随机0~100的数
+            //使用冒泡排序进行升序排序并打印
+            //使用冒泡排序进行降序排序并打印
+
+            int[] arr = new int[20];
+            Random r = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(0, 101);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+            //套路写法
+            //两层循环
+            //外层轮数
+            bool isSort = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                isSort = false;
+                //内层比较
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    //两值比较
+                    //满足交换
+                    // 降序还是升序 改这的比较条件就行了
+                    if (arr[j] < arr[j + 1])
+                    {
+                        isSort = true;
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+                if (!isSort)
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            #endregion
+
+            #region 练习题二
+            //写一个函数，实现一个数组的排序，并返回结果。可以根据参数决定是升序还是降序
+            Console.WriteLine();
+            int[] arr2 = { 3, 7, 2, 1, 9, 8, 6, 5, 4 };
+
+            Sort(arr2, false);
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+            #endregion
+        }
+
+        static int[] Sort(int[] array, bool isAscendingOrder)
+        {
+            //小优化 在循环外申明变量 可以提高一些性能
+            bool order;
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    //根据传入的 升序降序类型 决定使用什么规则 然后返回给一个bool值
+                    order = isAscendingOrder ? array[j] > array[j + 1] : array[j] < array[j + 1];
+                    if (order)
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ lesson14_选择排序/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+
+namespace Lesson14_选择排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("选择排序");
+
+            #region 知识点一 选择排序基本原理
+            // 8 7 1 5 4 2 6 3 9
+            // 新建中间商
+            // 依次比较
+            // 找出极值（最大或最小）
+            // 放入目标位置
+            // 比较n轮
+            #endregion
+
+            #region 知识点二 代码实现
+            //实现升序 把 大的 放在最后面
+            int[] arr = new int[] { 8, 7, 1, 5, 4, 2, 6, 3, 9 };
+
+            ////第一步 申明一个中间商 来记录索引
+            ////每一轮开始 默认第一个都是极值
+            //int index = 0;
+            ////第二步
+            ////依次比较
+            //for (int n = 1; n < arr.Length; n++)
+            //{
+            //    //第三步
+            //    //找出极值（最大值）
+            //    if( arr[index] < arr[n] )
+            //    {
+            //        index = n;
+            //    }
+            //}
+
+            ////第四步 放入目标位置
+            ////Length - 1 - 轮数
+            ////如果当前极值所在位置 就是目标位置 那就没必要交换了
+            //if( index != arr.Length - 1 - 轮数 )
+            //{
+            //    int temp = arr[index];
+            //    arr[index] = arr[arr.Length - 1 - 轮数];
+            //    arr[arr.Length - 1 - 轮数] = temp;
+            //}
+
+            //第五步 比较m轮
+            int index;
+            int temp;
+            for (int m = 0; m < arr.Length; m++)
+            {
+                //第一步 申明一个中间商 来记录索引
+                //每一轮开始 默认第一个都是极值
+                index = 0;//不要忘了 每一轮开始 都要重置中间商的值
+
+                //如果是
+                //index = m;
+
+                //第二步
+                //依次比较
+                // -m的目的 是排除上一轮 已经放好位置的数
+                for (int n = 1; n < arr.Length - m; n++)
+                {
+                    //第三步
+                    //找出极值（最大值）
+                    if (arr[index] < arr[n])
+                    {
+                        index = n;
+                    }
+                }
+
+                //第四步 放入目标位置
+                //Length - 1 - 轮数
+                //如果当前极值所在位置 就是目标位置 那就没必要交换了
+                if (index != arr.Length - 1 - m)//这里写成 m 是一样的 因为 m 和 arr.Length - 1 - m 是对称的
+                {
+                    temp = arr[index];
+                    arr[index] = arr[arr.Length - 1 - m];
+                    arr[arr.Length - 1 - m] = temp;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            #endregion
+
+            #region 另外一种选择排序
+            int[] arr1 = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int index1 = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] > arr[index1])
+                    {
+                        index = j;
+                    }
+
+                }
+                Console.WriteLine("index: " + arr[index1]);
+                if (index1 != i)
+                {
+                    int temp1 = arr[index1];
+                    arr[index1] = arr[i];
+                    arr[i] = temp1;
+                }
+
+            }
+
+
+            Console.WriteLine("Sorted array: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            #endregion
+
+            Console.WriteLine("选择排序练习题");
+            #region 练习题一
+            //定义一个数组，长度为20，每个元素值随机0~100的数
+            //使用选择排序进行升序排序并打印
+            //使用选择排序进行降序排序并打印
+
+            int[] array = new int[20];
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(0, 101);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            //套路写法
+            //两层循环
+            //外层轮数
+            for (int i = 0; i < array.Length; i++)
+            {
+                //初始索引
+                index = 0;
+                //内层寻找
+                for (int j = 1; j < array.Length - i; j++)
+                {
+                    //记录极值
+                    //这里决定了 找到极值的种类 
+                    //只需要改这的条件 就可以改变 降序还是升序
+                    if (array[index] > array[j])
+                    {
+                        index = j;
+                    }
+                }
+
+                //内层循环外交换
+                if (index != array.Length - 1 - i)
+                {
+                    temp = array[index];
+                    array[index] = array[array.Length - 1 - i];
+                    array[array.Length - 1 - i] = temp;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            #endregion
+
+            //总结
+            //基本概念
+            // 新建中间商
+            // 依次比较
+            // 找出极值
+            // 放入目标位置
+            // 比较n轮
+
+            //套路写法
+            //两层循环
+            //外层轮数
+            //内层寻找
+            //初始索引
+            //记录极值
+            //内存循环外交换
+            
+        }
+    }
+}
